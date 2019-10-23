ed69d4dd963248545a710ce23a27b9e6b6639716
Salvador González Gerpe <Salva@mac.local>
2019-10-23 17:08:06 +0200

    html path corrected

--- TDATA2RDFANDV/converter/views.py
+++ TDATA2RDFANDV/converter/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return render(request,'/home/salva/epw2rdf-contents/TDATA2RDFANDV/converter/templates/converter/index.html')
+	return render(request,'converter/index.html')
